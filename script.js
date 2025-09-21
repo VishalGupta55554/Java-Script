b16cdd23d32ed068a7393c57c5ecfd6de7725c85
VishalGupta55554 <vishalgupta2432002@gmail.com>
2025-09-21 13:29:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//const accountId = 5463;
+let accountName = "State Bank Of India";
+var acountType = "Saving";
+accountBrance = "Gaushala Crossing";
+let accountState;
+
+accountId = 1234;
+//console.table([accountId, accountName ,acountType]);
+console.log(accountId);
